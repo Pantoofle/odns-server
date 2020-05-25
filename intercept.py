d6fb513f90f4a9fbabf705ae2d120cea58d97ff8
Simon Fernandez <simon.fernandez@ens-lyon.fr>
2020-05-25 13:15:44 +0200

    Fix message parsing and crypto key management

--- intercept.py
+++ intercept.py
@@ -4,12 +4,12 @@
 """
 from __future__ import print_function
 
-import binascii,copy,socket,struct,sys
+import socket
 
-from dnslib import DNSRecord,RR,QTYPE,RCODE,parse_time
-from dnslib.server import DNSServer,DNSHandler,BaseResolver,DNSLogger
-from dnslib.label import DNSLabel
+from dnslib import DNSRecord, RCODE
+from dnslib.server import DNSServer, DNSHandler, BaseResolver, DNSLogger
 from odns import ODNSCypher
+
 
 class ODNSInterceptResolver(BaseResolver):
 
@@ -19,117 +19,120 @@ class ODNSInterceptResolver(BaseResolver):
         Proxy requests to upstream server and decyphering ODNS requests
     """
 
-    def __init__(self,address,port,decypher,skip,timeout=0):
+    def __init__(self, address, port, skip, key_path, timeout=0):
         """
             address/port    - upstream server
-            decypher        - list of wildcard labels to decypher with ODNS
             skip            - list of wildcard labels to skip
             timeout         - timeout for upstream server
+            key_path        - path to server secret key
         """
         self.address = address
         self.port = port
         self.skip = skip
         self.timeout = timeout
-        self.decypher = decypher
-        self.crypto = ODNSCypher()
 
-    def resolve(self,request,handler):
+        self.server_sk = None
+        with open(key_path, 'rb') as f:
+            self.server_sk = f.read()
+
+        self.crypto = ODNSCypher(server_sk=self.server_sk)
+
+    def resolve(self, request, handler):
         reply = request.reply()
         qname = request.q.qname
         print("QNAME : ", qname)
-        qtype = QTYPE[request.q.qtype]
-        # Test if we match ODNS and not skip
+
+        # Test if we match skip
         if not any([qname.matchGlob(s) for s in self.skip]):
-            if any([qname.matchGlob(s) for s in self.decypher]):
-                # Decypher the query and replace with clear qname
-                crypted = qname.stripSuffix("odns")
-                crypted = str(crypted)[:-1] # Remove the trailing.
-                crypted = bytes.fromhex(crypted) # Convert to bytes
+            # Decypher the query and replace with clear qname
+            crypted = qname.stripSuffix("odns")
+            crypted = str(crypted)[:-1]  # Remove the trailing.
+            crypted = bytes.fromhex(crypted)  # Convert to bytes
 
-                clear_request = self.crypto.decrypt(crypted)
+            clear_request, aes_key = self.crypto.decrypt_query(crypted)
 
-                request.q.qname = clear_request
-                # Forward it to the upstream server
-                try:
-                    if handler.protocol == 'udp':
-                        proxy_r = request.send(self.address,self.port,
-                                        timeout=self.timeout)
-                    else:
-                        proxy_r = request.send(self.address,self.port,
-                                        tcp=True,timeout=self.timeout)
-                    # Cypher back the reply
-                    reply = DNSRecord.parse(proxy_r)
-                    reply.q.qname = self.crypto.encrypt(reply.q.qname.encode(), qname.encode())
-                except socket.timeout:
-                    reply.header.rcode = getattr(RCODE,'NXDOMAIN')
+            request.q.qname = clear_request
+            # Forward it to the upstream server
+            try:
+                if handler.protocol == 'udp':
+                    proxy_r = request.send(self.address, self.port,
+                                           timeout=self.timeout)
+                else:
+                    proxy_r = request.send(self.address, self.port,
+                                           tcp=True, timeout=self.timeout)
+                # Cypher back the reply
+                reply = DNSRecord.parse(proxy_r)
+                reply.q.qname = self.crypto.encrypt_answer(
+                    reply.q.qname.encode(), aes_key)
+            except socket.timeout:
+                reply.header.rcode = getattr(RCODE, 'NXDOMAIN')
 
         # Else, just proxy
         if not reply.rr:
             try:
                 if handler.protocol == 'udp':
-                    proxy_r = request.send(self.address,self.port,
-                                    timeout=self.timeout)
+                    proxy_r = request.send(self.address, self.port,
+                                           timeout=self.timeout)
                 else:
-                    proxy_r = request.send(self.address,self.port,
-                                    tcp=True,timeout=self.timeout)
+                    proxy_r = request.send(self.address, self.port,
+                                           tcp=True, timeout=self.timeout)
                 reply = DNSRecord.parse(proxy_r)
             except socket.timeout:
-                reply.header.rcode = getattr(RCODE,'NXDOMAIN')
+                reply.header.rcode = getattr(RCODE, 'NXDOMAIN')
 
         return reply
 
+
 if __name__ == '__main__':
 
-    import argparse,sys,time
+    import argparse
+    import time
 
     p = argparse.ArgumentParser(description="ODNS Proxy")
-    p.add_argument("--port","-p",type=int,default=53,
-                    metavar="<port>",
-                    help="Local proxy port (default:53)")
-    p.add_argument("--address","-a",default="",
-                    metavar="<address>",
-                    help="Local proxy listen address (default:all)")
-    p.add_argument("--upstream","-u",default="8.8.8.8:53",
-            metavar="<dns server:port>",
-                    help="Upstream DNS server:port (default:8.8.8.8:53)")
-    p.add_argument("--tcp",action='store_true',default=False,
-                    help="TCP proxy (default: UDP only)")
-    p.add_argument("--decypher","-i",action="append",
-                    metavar="<label>",
-                    help="Decypher matching label (glob)")
-    p.add_argument("--skip","-s",action="append",
-                    metavar="<label>",
-                    help="Don't intercept matching label (glob)")
-    p.add_argument("--timeout","-o",type=float,default=5,
-                    metavar="<timeout>",
-                    help="Upstream timeout (default: 5s)")
-    p.add_argument("--log",default="request,reply,truncated,error",
-                    help="Log hooks to enable (default: +request,+reply,+truncated,+error,-recv,-send,-data)")
-    p.add_argument("--log-prefix",action='store_true',default=False,
-                    help="Log prefix (timestamp/handler/resolver) (default: False)")
+    p.add_argument("--port", "-p", type=int, default=5553,
+                   metavar="<port>",
+                   help="Local proxy port (default:5553)")
+    p.add_argument("--address", "-a", default="",
+                   metavar="<address>",
+                   help="Local proxy listen address (default:all)")
+    p.add_argument("--upstream", "-u", default="8.8.8.8:53",
+                   metavar="<dns server:port>",
+                   help="Upstream DNS server:port (default:8.8.8.8:53)")
+    p.add_argument("--tcp", action='store_true', default=False,
+                   help="TCP proxy (default: UDP only)")
+    p.add_argument("--skip", "-s", action="append",
+                   metavar="<label>",
+                   help="Don't intercept matching label (glob)")
+    p.add_argument("--timeout", "-o", type=float, default=5,
+                   metavar="<timeout>",
+                   help="Upstream timeout (default: 5s)")
+    p.add_argument("--key", "-k", default="./key.pem",
+                   metavar="<key_path>",
+                   help="Path to the server secret key")
+    p.add_argument("--log", default="request,reply,truncated,error",
+                   help="Log hooks to enable (default: +request,+reply,+truncated,+error,-recv,-send,-data)")
+    p.add_argument("--log-prefix", action='store_true', default=False,
+                   help="Log prefix (timestamp/handler/resolver) (default: False)")
     args = p.parse_args()
 
-    args.dns,_,args.dns_port = args.upstream.partition(':')
+    args.dns, _, args.dns_port = args.upstream.partition(':')
     args.dns_port = int(args.dns_port or 53)
 
     resolver = ODNSInterceptResolver(args.dns,
                                      args.dns_port,
-                                     args.decypher or [],
                                      args.skip or [],
+                                     args.key,
                                      args.timeout)
-    logger = DNSLogger(args.log,args.log_prefix)
+    logger = DNSLogger(args.log, args.log_prefix)
 
     print("Starting ODNS Poxy (%s:%d -> %s:%d) [%s]" % (
-                        args.address or "*",args.port,
-                        args.dns,args.dns_port,
-                        "UDP/TCP" if args.tcp else "UDP"))
+        args.address or "*", args.port,
+        args.dns, args.dns_port,
+        "UDP/TCP" if args.tcp else "UDP"))
 
-    for d in resolver.decypher:
-        print("    | ",d,sep="")
     if resolver.skip:
-        print("    Skipping:",", ".join(resolver.skip))
+        print("    Skipping:", ", ".join(resolver.skip))
     print()
-
 
     DNSHandler.log = {
         'log_request',      # DNS Request
@@ -154,4 +157,3 @@ if __name__ == '__main__':
 
     while udp_server.isAlive():
         time.sleep(1)
-
--- odns.py
+++ odns.py
@@ -1,45 +1,65 @@
-from ecies.utils import generate_key
-from ecies import encrypt as ECIESencrypt
-from ecies import decrypt as ECIESdecrypt
-# from coincurve import PublicKey
+from coincurve import PrivateKey, PublicKey
+from ecies.utils import generate_key, hex2pub
+from ecies.utils import encapsulate, decapsulate, aes_encrypt, aes_decrypt
+
 
 class ODNSCypher():
     """
-        Holding all cryptography informations (keys, nonces, algorithms) to
-        decypher requests and cypher back replies.
-
-        Using ECIES algorithm
+        Holding all cryptography algorithms to encrypt and decrypt queries and
+        answers.
     """
 
-    def __init__(self, sk=None):
-        """
-            sk  - Private (secret) Key of the proxy
-        """
+    def __init__(self, server_sk=None, server_pk=None):
+        if server_sk is not None:
+            self.server_sk = PrivateKey.from_pem(server_sk)
 
-        self.sk = sk
-        self.pk = None
+        self.server_sk = server_sk
 
-        if not self.sk:
-            print("No secret key given. Generating a new one...")
-            self.generate_key_pair()
+        if server_pk is not None:
+            if isinstance(server_pk, str):
+                self.server_pk = hex2pub(server_pk)
+            elif isinstance(server_pk, bytes):
+                self.server_pk = PublicKey(server_pk)
+            else:
+                raise TypeError("Invalid public key type")
 
-    def generate_key_pair(self):
-        secp_k = generate_key()
-        self.sk = secp_k.secret
-        self.pk = secp_k.public_key.format(True)
-        print("New pk : ", self.pk.hex())
+        self.server_pk = server_pk
 
-    def decrypt(self, name):
+    def encrypt_query(self, query: bytes):
         """
-            name - data to decrypt
-        """
-        return ECIESdecrypt(self.sk, name)
+            Encrypt the query to send to the server
 
-    def encrypt(self, name, original_request):
+            query     - The query
         """
-            name              - data to encrypt
-            original_request  - request made by client. Containing its public key
+        # Generate the ephemeral key
+        ephemeral_key = generate_key()
+
+        # Generate the symetric key
+        aes_key = encapsulate(ephemeral_key, self.server_pk)
+        # Encrypt the query (this adds the nonce)
+        cipher_text = aes_encrypt(aes_key, query)
+        # Return the message and the symetric key
+        return ephemeral_key.public_key.format(False) + cipher_text, aes_key
+
+    def decrypt_query(self, query: bytes):
         """
-        # Extract the sender key from the request
-        sender_pk = original_request[:65]
-        return ECIESencrypt(sender_pk, name)
+            Decrypts a query received by the server
+
+            query - The query to decrypt
+        """
+        # Parse the server key
+
+        # Parsre the msg, extract the pubkey and the {IV || payload} from the query
+        pubkey = query[0:65]  # uncompressed pubkey's length is 65 bytes
+        encrypted = query[65:]
+        ephemeral_public_key = PublicKey(pubkey)
+
+        # Generate the AES key
+        aes_key = decapsulate(ephemeral_public_key, self.server_sk)
+        return aes_decrypt(aes_key, encrypted), aes_key
+
+    def encrypt_answer(self, answer: bytes, aes_key: bytes):
+        return aes_encrypt(aes_key, answer)
+
+    def decrypt_answer(self, answer: bytes, aes_key: bytes):
+        return aes_decrypt(aes_key, answer)
